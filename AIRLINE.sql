52a11368d3d129ff08bc7769ea9f94b583dce12e
chrisbrunet <132419309+chrisbrunet@users.noreply.github.com>
2023-11-28 20:25:36 -0700

    continuing to create database in sql file

--- AIRLINE.sql
+++ AIRLINE.sql
@@ -3,16 +3,50 @@ CREATE DATABASE AIRLINE;
 USE AIRLINE;
 
 
-DROP TABLE IF EXISTS STUDIO;
-CREATE TABLE STUDIO (
-	Name			varchar(25) not null,
-	Phone			char(12),
-	Address			varchar(25),
-	primary key (Name)
+DROP TABLE IF EXISTS CREW;
+CREATE TABLE CREW (
+	crewID	varchar(1) not null,
+	primary key (crewID)
 );
 
-INSERT INTO STUDIO (Name, Phone, Address)
+INSERT INTO CREW (crewID)
 VALUES
-('Music Mastery',	'403-357-4457',	'25 Heron Way NW'),
-('Melody Time',	'587-594-7593',	'124 Watford Rise SE'),
-('Harmony Inc.',	'403-954-5232',	'68 Cedar Drive NE');
+('A'), ('B'), ('C'), ('D'), ('E'), ('F');
+
+DROP TABLE IF EXISTS AIRCRAFT;
+CREATE TABLE AIRCRAFT (
+	aircraftID int not null auto_increment,
+    crewID varchar(1) not null,
+    aircraftType varchar(10) not null,
+	primary key (aircraftID), 
+    foreign key (crewID) references CREW(crewID)
+);
+
+INSERT INTO AIRCRAFT (crewID, aircraftType)
+VALUES
+('A', '737'), 
+('B', '737'), 
+('C', '787'), 
+('D', '787'), 
+('E', '747'), 
+('F', 'F-16');
+
+DROP TABLE IF EXISTS FLIGHT;
+CREATE TABLE FLIGHT (
+	flightID int not null auto_increment,
+    aircraftID int not null,
+    departCity varchar(20) not null,
+	arriveCity varchar(20) not null,
+    flightDate date not null,
+	primary key (flightID), 
+    foreign key (aircraftID) references AIRCRAFT(aircraftID)
+);
+
+INSERT INTO FLIGHT (aircraftID, departCity, arriveCity, flightDate)
+VALUES
+(1, 'Calgary', 'Toronto', '2023-12-05'), 
+(2, 'Toronto', 'Calgary', '2023-12-05'), 
+(3, 'Calgary', 'Vancouver', '2023-12-05'), 
+(4, 'Vancouver', 'Calgary', '2023-12-05'), 
+(5, 'Vancouver', 'Toronto', '2023-12-05'), 
+(6, 'Toronto', 'Vancouver', '2023-12-05');
